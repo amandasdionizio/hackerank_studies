81dd8bf26101ad9ab44f1f62d96916ab7d7ec401
amandasdionizio <amanda.salgadodionizio@gmail.com>
2024-06-18 13:48:17 -0300

    exercise about divisibleSumPairs function

---
+++ hackerrank/divisibleSumPairs.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    counter = 0
+    for verification in range(len(ar)):
+        for comparate in range(verification + 1, len(ar)):
+            if (ar[verification] + ar[comparate]) % k == 0:
+                counter += 1
+    return counter
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
